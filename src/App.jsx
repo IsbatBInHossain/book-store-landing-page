e90fd7e0a595b6649c25403abc23126a6e0a9712
Isbat-Bin-Hossain <isbat31416@gmail.com>
2023-10-07 22:01:38 +0600

    Add NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import NavBar from './components/Nav'
 
 export default function App() {
   return (
-    <body className=' w-full h-screen flex flex-col justify-center items-center font-sono '>
+    <section className=' w-full h-screen flex flex-col justify-center items-center font-sono '>
       <NavBar />
       <main className=' bg-[#e37754] w-full h-full'>Body</main>
-    </body>
+    </section>
   )
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,41 @@
+import LogoSvg from '../assets/Logo.svg'
+import bagSvg from '../assets/bag.svg'
+
 const NavBar = () => {
   return (
-    <nav className='bg-[#ffc7a9] w-full h-20 flex justify-between items-center'>
-      <div className='ml-3 flex gap-3'>
-        <div className=''>Logo</div>
-        <h1>Banbury Cross</h1>
+    <nav className='bg-[#ffc7a9] w-full h-24 flex justify-between items-center shadow-2xl'>
+      <div className='ml-8 flex gap-3'>
+        <div className=' w-12 h-12'>
+          <img src={LogoSvg} alt='logo' />
+        </div>
+        <h1 className=' font-ultrabold tracking-tighter flex flex-col'>
+          <span>Banbury</span> Cross
+        </h1>
+      </div>
+      <div className=''>
+        <ul className='flex justify-between gap-5 font-extrabold'>
+          <li>
+            <a href='#'>Home</a>
+          </li>
+          <li>
+            <a href='#'>bookstore</a>
+          </li>
+          <li>
+            <a href='#'>Blog</a>
+          </li>
+          <li>
+            <a href='#'>my Account</a>
+          </li>
+          <li>
+            <a href='#'>Contact</a>
+          </li>
+        </ul>
+      </div>
+      <div className=' w-6 h-6 mr-8'>
+        <img src={bagSvg} alt='shopping icon' />
       </div>
     </nav>
   )
 }
+
 export default NavBar
